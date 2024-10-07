74c5ac1057e153ed461aac79193032c04fcd6d2e
Sahani18 <sahani.subhanshu1995@gmail.com>
2024-10-07 13:03:03 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { routing } from "./routing";
+import "./index.css";
 
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { routing } from './routing'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <RouterProvider router={routing} />
-  
- 
-)
+);
