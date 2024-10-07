ccba4eb9dc18c0da3a2253748c27ecd04aa6e4dc
Sahani18 <sahani.subhanshu1995@gmail.com>
2024-10-07 13:04:50 +0530

    GA fixes

--- src/analytics/googleAnalytics.js
+++ src/analytics/googleAnalytics.js
@@ -2,24 +2,23 @@ import ReactGA from "react-ga4";
 import { GA_TRACKING_ID } from "../utils/constants";
 
 export const initialiseGA = () => {
-    if (!GA_TRACKING_ID) {
-        console.log("Tracking ID not found")
-        return;
-    }
-    ReactGA.initialize(GA_TRACKING_ID);
-}
+  if (!GA_TRACKING_ID) {
+    console.log("Tracking ID not found");
+    return;
+  }
+  ReactGA.initialize(GA_TRACKING_ID);
+};
 
 // track page view
 export const trackPageView = (screenName) => {
-    ReactGA.send({ hitType: "pageview", page: screenName, });
-}
+  ReactGA.send({ hitType: "pageview", page: screenName });
+};
 
 // track click event
 export const trackClickEvent = (catagory, label) => {
-    ReactGA.event({
-        catagory,
-        label,
-        action: "click"
-
-    });
-}
+  ReactGA.event({
+    catagory,
+    label,
+    action: "click",
+  });
+};
