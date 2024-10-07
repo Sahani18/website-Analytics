2ae60969be03ad90afd6a614cc8d181b143761e4
Sahani18 <sahani.subhanshu1995@gmail.com>
2024-10-07 15:54:11 +0530

    GA initialization location changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { Link } from 'react-router-dom'
-import './App.css'
-import Button from './components/button'
-import { useEffect } from 'react'
-import { initialiseGA } from './analytics/googleAnalytics'
+import { Link } from "react-router-dom";
+import Button from "./components/button";
+
+import "./App.css";
 
 function App() {
-  
-  useEffect(()=>{
-    initialiseGA()
-  },[])
-
   return (
     <>
-      <p className='mx-auto text-green-700 text-4xl pt-20'>Hello Google Analytics...</p>
-      <p className='pt-20'>Click below to navigate to homepage</p>
-      <br/>
-      <Link to={"/home"} className=' bg-yellow-200 rounded-full p-2'> Home Page</Link>
-      <div className='h-32'/>
+      <p className="mx-auto text-green-700 text-4xl pt-20">
+        Hello Google Analytics...
+      </p>
+      <p className="pt-20">Click below to navigate to homepage</p>
+      <br />
+      <Link to={"/home"} className=" bg-yellow-200 rounded-full p-2">
+        {" "}
+        Home Page
+      </Link>
+      <div className="h-32" />
       <Button category="User Interaction" label="Click" />
-        </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/routing.jsx
+++ src/routing.jsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./components/home";
+import { initialiseGA } from "./analytics/googleAnalytics";
 
+(()=>{
+    initialiseGA()
+})()
 export const routing = createBrowserRouter([
     {
         path: "/",
